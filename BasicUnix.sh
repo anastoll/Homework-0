2bb7435431f8a7f7922823198acc380b37aae010
anastoll <anayansi.stoll@gmail.com>
2020-11-14 13:09:47 +0100

    Basic Unix scrip file

---
+++ BasicUnix.sh
@@ -0,0 +1,9 @@
+cd ~/Documents    # move to home directory + Documents
+mkdir projects    # make a new directory called projects
+cd projects    # move to ~/projects directory
+mkdir murders    # make new directory called murders inside of projects
+cd murders    # move to ~/projects/murders/
+mkdir data rda    # make two new directories, one is data the other is rda folder
+ls    # to check if we indeed have one data folder and one rda folder
+pwd    # check the current working directory
+mkdir figs    # make a directory called figs to store figures
